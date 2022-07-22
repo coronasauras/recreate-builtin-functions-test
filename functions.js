f0041ff1505ab6940653d696c623753891546703
coronasauras <harry.chenjun.jing@gmail.com>
2022-07-22 12:04:13 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,30 @@
 // DEFINE YOUR FUNCTIONS HERE BASED ON THE PROVIDED CRITERIA
+
+//Include function
+function includes(item, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Index of item function
+function indexOf(item, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//Reverse function
+function reverse(array) {
+  let arrayReverse = [];
+  while (array.length) {
+    arrayReverse.push(array.pop());
+  }
+  return arrayReverse;
+}
